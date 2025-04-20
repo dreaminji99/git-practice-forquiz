e29a5e48e9fd68ba755f9166c924e8e80658ec99
dreaminji <21-11112mj@sookmyung.ac.kr>
2025-04-21 02:26:22 +0900

    오류나면 메세지 띄우는 것으로 수정함

--- hello.py
+++ hello.py
@@ -1,7 +1,15 @@
 print("안녕하세요!")
-f = open("data.txt", "r")
-lines = f.readlines()
-for line in lines:
-    print(line)
-f.close()
+#f = open("data.txt", "r")
+#lines = f.readlines()
+#for line in lines:
+#    print(line)
+#f.close()
 #오류 수정 안 했는데 일단 이걸 적고 깃허브로 add 되는지 확인할거임. 
+try:
+    f = open("data.txt", "r")
+    lines = f.readlines()
+    for line in lines:
+        print(line)
+    f.close()
+except FileNotFoundError:
+    print("파일이 존재하지 않습니다. data.txt가 같은 폴더에 있는지 확인하세요.")
